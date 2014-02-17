bbfd6557061fef2af8c9f3745d391e44bf5cffab
Brad <broswen@gmail.com>
2014-02-17 11:59:19 -0600

    added more listeners

--- src/me/broswen/banvil/PlayerListener.java
+++ src/me/broswen/banvil/PlayerListener.java
@@ -2,10 +2,15 @@ package me.broswen.banvil;
 
 
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PlayerListener implements Listener{
@@ -20,6 +25,10 @@ public class PlayerListener implements Listener{
 			return;
 		}
 		
+		if(event.getAction() != Action.RIGHT_CLICK_BLOCK){
+			return;
+		}
+		
 		if(!player.hasPermission("banvil.use")){
 			if(event.getClickedBlock().getType() == Material.ANVIL){
 				event.setCancelled(true);
@@ -29,9 +38,58 @@ public class PlayerListener implements Listener{
 	}
 	
 	@EventHandler
+	public void onBlockBreak(BlockBreakEvent event){
+		Player player = event.getPlayer();
+		plugin = BAnvil.plugin;
+		
+		if(event.getBlock() == null){
+			return;
+		}
+		
+		if(event.getBlock().getType() == Material.ANVIL){
+			
+			if(!player.hasPermission("banvil.break")){
+				event.setCancelled(true);
+				player.sendMessage(plugin.prefix + "You are not permitted to break anvils!");
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onBlockPlace(BlockPlaceEvent event){
+		Player player = event.getPlayer();
+		plugin = BAnvil.plugin;
+		
+		if(event.getBlock() == null){
+			return;
+		}
+		
+		if(event.getBlock().getType() == Material.ANVIL){
+			
+			if(!player.hasPermission("banvil.place")){
+				event.setCancelled(true);
+				player.sendMessage(plugin.prefix + "You are not permitted to place anvils!");
+			}
+		}
+	}
+	
+	@EventHandler
 	public void onBlockDamage(BlockDamageEvent event){
 		Player player = event.getPlayer();
+		plugin = BAnvil.plugin;
+		FileConfiguration config = plugin.getConfig();
 		
+		if(event.getBlock() == null){
+			return;
+		}
+		
+		if(!config.getBoolean("infinite")){
+			return;
+		}
+		
+		if(event.getBlock().getType() == Material.ANVIL){
+			event.setCancelled(true);
+		}
 	}
 	
 	
