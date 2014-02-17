baf5b3eed60336e5af7c04a03b0d6bbd046eb1c3
Brad <broswen@gmail.com>
2014-02-17 10:56:48 -0600

    added info command

--- src/me/broswen/banvil/BAnvil.java
+++ src/me/broswen/banvil/BAnvil.java
@@ -1,15 +1,20 @@
 package me.broswen.banvil;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BAnvil extends JavaPlugin{
 	public static BAnvil plugin;
+	public String prefix = ChatColor.DARK_GRAY + "[BAnvil] " + ChatColor.RESET;
 	
 	@Override
 	public void onEnable(){
-		
+		loadConfig();
+		this.plugin = this;
 	}
 	
 	@Override
@@ -18,6 +23,36 @@ public class BAnvil extends JavaPlugin{
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String args[]){
+		PluginDescriptionFile pdfFile = this.getDescription();
+		
+		if(cmd.getName().equalsIgnoreCase("banvil")){
+			if(!sender.hasPermission("banvil.info")){
+				sender.sendMessage(ChatColor.GRAY + "You don't have permission!");
+				return true;
+			}
+			
+			if(sender instanceof Player){
+				Player player = (Player) sender;
+				
+				player.sendMessage(ChatColor.DARK_GRAY.toString() + ChatColor.BOLD + "====== BAnvil ======");
+				player.sendMessage("Version: " + pdfFile.getVersion());
+				player.sendMessage("Author: " + pdfFile.getAuthors());
+				player.sendMessage("About: " + pdfFile.getDescription());
+				player.sendMessage(ChatColor.DARK_GRAY.toString() + ChatColor.BOLD + "====== BAnvil ======");
+			}else{
+				getLogger().info("====== BAnvil ======");
+				getLogger().info("Version: " + pdfFile.getVersion());
+				getLogger().info("Author: " + pdfFile.getAuthors());
+				getLogger().info("About: " + pdfFile.getDescription());
+				getLogger().info("====== BAnvil ======");
+			}
+		}
 		return true;
+	}
+	
+	public void loadConfig(){
+		saveDefaultConfig();
+		getConfig().options().copyDefaults(true);
+		saveConfig();
 	}
 }
